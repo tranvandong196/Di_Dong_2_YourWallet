02f15bef21ee96d026e7ad7243a9a772241fa9c4
tranvandong196 <Superhihiha@gmail.com>
2017-05-31 02:41:42 +0700

    Thêm giao diện sổ giao dịch
    - Xem tổng quan số dư
    - Danh sách giao dịch
    - SwipeLeft/Right để thay đổi ngày đang xem
    -> Chưa code chức năng

---
+++ YourWallet/Controller/BookHeaderCell.swift
@@ -0,0 +1,24 @@
+//
+//  BookHeaderCell.swift
+//  YourWallet
+//
+//  Created by Tran Van Dong on 30/5/17.
+//  Copyright © 2017 Tran Van Dong. All rights reserved.
+//
+
+import UIKit
+
+class BookHeaderCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- YourWallet/Controller/BookViewController.swift
+++ YourWallet/Controller/BookViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
-class BookViewController: UIViewController {
+class BookViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
+    @IBOutlet weak var previousDay_Button: UIButton!
+    @IBOutlet weak var currentDay_Button: UIButton!
+    @IBOutlet weak var nextDay_Button: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -25,7 +28,47 @@ class BookViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBAction func SwipeRight_Gesture(_ sender: Any) {
+        previousDay_Button.setTitle("29/05/2017", for: .normal)
+        currentDay_Button.setTitle("HÔM QUA", for: .normal)
+        nextDay_Button.setTitle("HÔM NAY", for: .normal)
+        
+        print("Thao tác vuốt phải thành công")
+    }
+    
+    @IBAction func SwipeLeft_Gesture(_ sender: Any) {
+        previousDay_Button.setTitle("HÔM NAY", for: .normal)
+        currentDay_Button.setTitle("NGÀY MAI", for: .normal)
+        nextDay_Button.setTitle("31/05/2017", for: .normal)
+        print("Thao tác vuốt trái thành công")
+    }
+    
+    
+    // MARK: ** TableView
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 4
+    }
+  
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let header = tableView.dequeueReusableCell(withIdentifier: "HeaderCell") as! BookHeaderCell
+        return header
+    }
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 95
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TransactionCell") as! BookTransactionCell
+        return cell
+        
+    }
+    
     /*
     // MARK: - Navigation
 
