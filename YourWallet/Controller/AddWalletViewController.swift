e4ed346776fe397b3e86ae26c2221b890e5c8c97
Nguyễn Đình Sơn <001.IceTea@gmail.com>
2017-06-10 22:47:44 +0700

    Sơ bộ màn hình thêm ví
    - Chưa cho chọn icon + đơn vị
    - Nhập empty sẽ crash

--- YourWallet/Controller/AddWalletViewController.swift
+++ YourWallet/Controller/AddWalletViewController.swift
@@ -8,13 +8,19 @@
 
 import UIKit
 
-class AddWalletViewController: UIViewController {
+class AddWalletViewController: UIViewController{
 
+    @IBOutlet weak var WalletIcon_ImageView: UIImageView!
+    @IBOutlet weak var WalletName_textField: UITextField!
+    @IBOutlet weak var currCurrency_btn: UIButton!
+    @IBOutlet weak var currAmount_txtField: UITextField!
+    
     @IBOutlet weak var Save_Button: UIBarButtonItem!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,7 +37,15 @@ class AddWalletViewController: UIViewController {
     }
     @IBAction func Save_ButtonTapped(_ sender: Any) {
         self.tabBarController?.tabBar.isHidden = isSelectWallet ? true: false
-        //Thao tác lưu ở đây
+        //Thao tác lưu ở đây        let walletName = WalletName_textField.text!
+        let moneyAmount = Double(currAmount_txtField.text!)!
+        let currency = currCurrency_btn.titleLabel!.text!
+        let walletIcon = "Vi-icon"
+        
+        let database = Connect_DB_SQLite(dbName: DBName, type: DBType)
+        let sqlQueryStr = "INSERT INTO ViTien (Ma,Ten,TienTe,TongGiaTri,Icon) VALUES (null, '\(walletName)', '\(currency)', \(moneyAmount), '\(walletIcon)')"
+        
+        Query(Sql: sqlQueryStr, database: database)
         self.navigationController?.popViewController(animated: true)
     }
     /*
