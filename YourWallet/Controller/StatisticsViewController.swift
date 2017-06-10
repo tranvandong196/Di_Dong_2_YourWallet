57745b8cc4e6eb958bfcf80c5dea0b3642c5be3b
Nguyễn Đình Sơn <001.IceTea@gmail.com>
2017-06-11 03:30:42 +0700

    Cơ bản màn hình thống kê
    Biểu đồ và animation

--- YourWallet/Controller/StatisticsViewController.swift
+++ YourWallet/Controller/StatisticsViewController.swift
@@ -7,13 +7,24 @@
 //
 
 import UIKit
+import Charts
 
 class StatisticsViewController: UIViewController {
 
+    @IBOutlet weak var pieChartView: PieChartView!
+    
     @IBOutlet weak var SelectWallet_Button: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun"]
+        let unitsSold = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0]
+        
+        
+        //pieChartView = PieChartView(frame: self.view.bounds)
+        //self.view.addSubview(pieChartView!)
+        setChart(dataPoints: months, values: unitsSold)
     }
     override func viewWillAppear(_ animated: Bool) {
         print("🖥 Thống kê --------------------------------")
@@ -45,5 +56,35 @@ class StatisticsViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    func setChart(dataPoints: [String], values: [Double]) {
+        
+        var dataEntries: [ChartDataEntry] = []
+        
+        for i in 0..<dataPoints.count {
+            let dataEntry1 = PieChartDataEntry(value: values[i], label: dataPoints[i])
+            dataEntries.append(dataEntry1)
+        }
+        
+        let pieChartDataSet = PieChartDataSet(values: dataEntries, label: "Units Sold")
+        pieChartDataSet.colors = ChartColorTemplates.colorful()
+        let pieChartData = PieChartData(dataSet: pieChartDataSet)
+        pieChartView.data = pieChartData
+        
+        var colors: [UIColor] = []
+        
+        for _ in 0..<dataPoints.count {
+            let red = Double(arc4random_uniform(256))
+            let green = Double(arc4random_uniform(256))
+            let blue = Double(arc4random_uniform(256))
+            
+            let color = UIColor(red: CGFloat(red/255), green: CGFloat(green/255), blue: CGFloat(blue/255), alpha: 1)
+            colors.append(color)
+        }
+        
+        
+        pieChartView.animate(xAxisDuration: 1.0, yAxisDuration: 1.0, easingOption: .easeInSine)
+        //pieChartView.chartDescription?.text = "des"
+    }
 
 }
