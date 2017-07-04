801b1d82804d2da784ec9e062eef4a37d9ebc742
Nguyen Dinh Vu <dinhvu@DinhVus-Mac.local>
2017-07-03 21:42:52 -0700

    Thêm chức năng chọn icon cho ví, nhóm
    Thêm chức năng chọn icon cho ví, nhóm

---
+++ YourWallet/Controller/SelectIconCell.swift
@@ -0,0 +1,14 @@
+//
+//  SelectIconCell.swift
+//  YourWallet
+//
+//  Created by DinhVu on 7/3/17.
+//  Copyright © 2017 Tran Van Dong. All rights reserved.
+//
+
+import UIKit
+
+class SelectIconCell: UICollectionViewCell {
+    
+    @IBOutlet weak var CategoryIcon_Imageview: UIImageView!
+}
--- YourWallet/Controller/SelectIconViewController.swift
+++ YourWallet/Controller/SelectIconViewController.swift
@@ -8,19 +8,19 @@
 
 import UIKit
 
-class SelectIconViewController: UIViewController {
+class SelectIconViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate{
 
+    @IBOutlet weak var Coll: UICollectionView!
+    var Icons = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
+        Coll.delegate = self
+        Coll.dataSource = self
         
-        iconName = "ATM-4-icon"
-        
-//        if let viewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "AddWalletVC") as? AddWalletViewController
-//        {
-//            viewController.iconName = "ATM-4-icon"
-//            print(viewController.iconName!)
-//        }
-        // Do any additional setup after loading the view.
+        let DB = Connect_DB_SQLite(dbName: DBName, type: DBType)
+        Icons = GetIconNameFromSQLite(query: "SELECT * FROM BieuTuong", database:  DB  )
+        sqlite3_close(DB)
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,6 +28,34 @@ class SelectIconViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    // MARK: *** CollectionView
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return Icons.count
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let Cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! SelectIconCell
+        Cell.CategoryIcon_Imageview.image = UIImage(named: Icons[indexPath.row])
+        return Cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        iconName =  Icons[indexPath.row]
+        self.navigationController?.popViewController(animated: true)
+    }
+    //  MARK: ***Fuction
+    func GetIconNameFromSQLite(query: String, database: OpaquePointer?) -> [String]{
+        var Icons = [String]()
+        var queryStatement:OpaquePointer? = nil
+        if sqlite3_prepare_v2(database, query, -1, &queryStatement, nil) == SQLITE_OK{
+            while sqlite3_step(queryStatement) == SQLITE_ROW{sqlite3_column_text(queryStatement, 1)
+                if sqlite3_column_text(queryStatement, 0) != nil {
+                    let n:String = String(cString: sqlite3_column_text(queryStatement, 0))
+                    Icons.append(n)
+                }
+            }
+        }
+        return Icons
+    }
 
     /*
     // MARK: - Navigation
